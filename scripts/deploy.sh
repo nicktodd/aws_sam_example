4d78d5c8cf00e6951a6145481da4421e9b1a57bc
Nick Todd <nick.todd@conygre.com>
2021-10-11 11:52:18 +0100

    Added the npm install test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,20 @@
+# Download the dependencies
+npm install
+
+# Run the tests
+npm test
+
+
+stackname=AzureDevopsAlbumStack
+
 echo Create the cloudformation package
 aws cloudformation package --template template.yml --s3-bucket aws-sam-cli-managed-default-samclisourcebucket-1kni8z5nxcykx --output-template template-export.yml
 
 echo Deploy the cloudformation package
-aws cloudformation deploy --template-file template-export.yml --stack-name AzureDevopsAlbumStack --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file template-export.yml --stack-name $stackname --capabilities CAPABILITY_IAM
 
-aws cloudformation wait stack-create-complete --stack-name AzureDevopsAlbumStack --region eu-west-1 
+#aws cloudformation wait stack-create-complete --stack-name $stackname --region eu-west-1 
 
-
-aws cloudformation describe-stack-events --stack-name AzureDevopsAlbumStack --region eu-west-1
+#aws cloudformation describe-stack-events --stack-name $stackname --region eu-west-1
 
 echo All done
